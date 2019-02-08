1e1461c0ae42d6feb95dcbfd48b6ffbf43e508ed
Prateek Srivastava <sandeep.tiwari@sizmek.com>
2019-02-08 17:35:05 +0530

    base routing setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Home from './home/components/home';
+import Login from './login/components/login';
+import ProductDetail from './productDetail/components/productDetail';
+import Register from './register/components/register';
+
 import './App.css';
 
-function App(){
-    return (
-      <div className="App">
+function App() {
+  return (
+    <div className="App">
 
-        App component
+      <div className="routeContainer">
+        <Switch>
+          <Route exact path="/" render={() => (<Redirect to="/home" />)} />
+          <Route path="/home" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/productDetail" component={ProductDetail} />
+          <Route path="/register" component={Register} />
+          <Redirect to="/" />
+        </Switch>
+
 
       </div>
-    );
+
+    </div>
+  );
 }
 
 export default App;
