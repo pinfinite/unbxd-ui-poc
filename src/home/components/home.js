ab485b664b69df1053601ea242f514b445ace3cf
Prateek Srivastava <sandeep.tiwari@sizmek.com>
2019-02-08 16:50:55 +0530

    home component added

---
+++ src/home/components/home.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import './home.css';
+export default class Home extends Component{
+    render(){
+        return(
+         <div>Home Component here--------></div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={createStore(rootReducer,applyMiddleware(ReduxPromise))}>
+  <BrowserRouter>
+    <Route path="/" component={App}/>
+  </BrowserRouter>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
