29a2b50555541d35531ff65ee7003135f049dd3a
Prateek Srivastava <sandeep.tiwari@sizmek.com>
2019-02-08 17:03:49 +0530

    register component added

---
+++ src/register/components/register.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import './register.css';
+
+export default class Register extends Component{
+    render(){
+        return(
+            <div>Register component here -------></div>
+        );
+    }
+}
