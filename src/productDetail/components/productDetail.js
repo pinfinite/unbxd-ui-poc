fbcb240dd3887f0b0dac9010609ba8a4146d23dc
Prateek Srivastava <sandeep.tiwari@sizmek.com>
2019-02-08 16:57:19 +0530

    product detail component added

---
+++ src/productDetail/components/productDetail.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import './productDetail.css';
+
+export default class ProductDetail extends Component{
+    render(){
+        return(
+            <div>Product Detail Component Here ----------></div>
+        );
+    }
+}
